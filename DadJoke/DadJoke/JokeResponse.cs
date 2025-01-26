ec98e7cfd4a8d4ad7ec70d5a7b71f8def912174d
Rod Falanga <rfalanga@users.noreply.github.com>
2025-01-25 17:29:49 -0700

    Got the basics of this app written

---
+++ DadJoke/DadJoke/JokeResponse.cs
@@ -0,0 +1,9 @@
+﻿// write a program that will tell a dad joke using the icanhazdadjoke API (https://icanhazdadjoke.com/api) and the HttpClient class.
+// The program should make a GET request to the API and then print the joke to the console.
+public class JokeResponse
+{
+    public string Id { get; set; }
+    public string Joke { get; set; }
+    public int Status { get; set; }
+}
+// The program should be written in a way that allows it to be easily extended to tell jokes from other joke APIs in the future.
--- DadJoke/DadJoke/Program.cs
+++ DadJoke/DadJoke/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// write a program that will tell a dad joke using the icanhazdadjoke API (https://icanhazdadjoke.com/api) and the HttpClient class.
+// The program should make a GET request to the API and then print the joke to the console.
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var joke = await GetDadJokeAsync();
+        Console.WriteLine(joke);
+    }
+
+    static async Task<string> GetDadJokeAsync()
+    {
+        using (var client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("DadJokeApp", "1.0"));
+
+            var response = await client.GetAsync("https://icanhazdadjoke.com/");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var jokeResponse = System.Text.Json.JsonSerializer.Deserialize<JokeResponse>(jsonString);
+                return jokeResponse.Joke;
+            }
+            else
+            {
+                return "Failed to retrieve a joke.";
+            }
+        }
+    }
+}
+// The program should be written in a way that allows it to be easily extended to tell jokes from other joke APIs in the future.
